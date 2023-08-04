1d5a66f608c8e1a6803f87d4854dcedfd7e2962a
Pankaj Bisht <Pankaj.Bisht2@rsystems.com>
2023-08-04 19:47:06 +0530

    Updated sql script

--- scripts/SqlScript.sql
+++ scripts/SqlScript.sql
@@ -1,49 +1,102 @@
-﻿--Create tables
-
+﻿USE [CouncilVotingDb]
+GO
+/****** Object:  Table [dbo].[Measures]   ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 CREATE TABLE [dbo].[Measures](
-	[Id] [INT] IDENTITY(1,1) PRIMARY KEY NOT NULL,
-	[Subject] [NVARCHAR](100) NOT NULL,
-	[Description] [NVARCHAR](MAX) NOT NULL,
-	[MinimumNoOfVotesRequired] [INT] NOT NULL,
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Subject] [nvarchar](100) NOT NULL,
+	[Description] [nvarchar](max) NOT NULL,
+	[MinimumNoOfVotesRequired] [int] NOT NULL,
 	[CreatedBy] [nvarchar](20) NULL,
 	[CreatedDate] [datetime] NULL,
 	[UpdatedBy] [nvarchar](20) NULL,
-	[UpdatedDate] [datetime] NULL
-)
-
+	[UpdatedDate] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Options]  ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 CREATE TABLE [dbo].[Options](
-	[Id] [INT] IDENTITY(1,1) PRIMARY KEY NOT NULL,
-	[MeasureId] INT FOREIGN KEY REFERENCES [dbo].[Measures](Id),
-	[Value] [NVARCHAR](100) NOT NULL,
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[MeasureId] [int] NULL,
+	[Value] [nvarchar](100) NOT NULL,
 	[CreatedBy] [nvarchar](20) NULL,
 	[CreatedDate] [datetime] NULL,
 	[UpdatedBy] [nvarchar](20) NULL,
-	[UpdatedDate] [datetime] NULL
-)
-
+	[UpdatedDate] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Users]     ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 CREATE TABLE [dbo].[Users](
-	[Id] [INT] IDENTITY(1,1) PRIMARY KEY NOT NULL,
-	[Name] [NVARCHAR](100) NOT NULL,
-	[Email] [NVARCHAR](100) NOT NULL,
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](100) NOT NULL,
+	[Email] [nvarchar](100) NOT NULL,
 	[CreatedBy] [nvarchar](20) NULL,
 	[CreatedDate] [datetime] NULL,
 	[UpdatedBy] [nvarchar](20) NULL,
-	[UpdatedDate] [datetime] NULL
-)
-
+	[UpdatedDate] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Votings]    ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 CREATE TABLE [dbo].[Votings](
-	[Id] [INT] IDENTITY(1,1) PRIMARY KEY NOT NULL,
-	[MeasureId] INT FOREIGN KEY REFERENCES [dbo].[Measures](Id),
-	[UserId] INT FOREIGN KEY REFERENCES [dbo].[Users](Id),
-	[OptionId] INT FOREIGN KEY REFERENCES [dbo].[Options](Id),
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[MeasureId] [int] NULL,
+	[UserId] [int] NULL,
+	[OptionId] [int] NULL,
 	[CreatedBy] [nvarchar](20) NULL,
 	[CreatedDate] [datetime] NULL,
 	[UpdatedBy] [nvarchar](20) NULL,
-	[UpdatedDate] [datetime] NULL
-)
-
---Seed data
-
-INSERT INTO Users ([Name], [Email]) VALUES
-('Ben', 'Ben@abc.com'),
-('Tom', 'Tom@abc.com')
+	[UpdatedDate] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+SET IDENTITY_INSERT [dbo].[Users] ON 
+GO
+INSERT [dbo].[Users] ([Id], [Name], [Email], [CreatedBy], [CreatedDate], [UpdatedBy], [UpdatedDate]) VALUES (1, N'Ben', N'Ben@abc.com', NULL, NULL, NULL, NULL)
+GO
+INSERT [dbo].[Users] ([Id], [Name], [Email], [CreatedBy], [CreatedDate], [UpdatedBy], [UpdatedDate]) VALUES (2, N'Tom', N'Tom@abc.com', NULL, NULL, NULL, NULL)
+GO
+INSERT [dbo].[Users] ([Id], [Name], [Email], [CreatedBy], [CreatedDate], [UpdatedBy], [UpdatedDate]) VALUES (3, N'Ren', N'Ren@xyz.com', NULL, NULL, NULL, NULL)
+GO
+SET IDENTITY_INSERT [dbo].[Users] OFF
+GO
+ALTER TABLE [dbo].[Options]  WITH CHECK ADD FOREIGN KEY([MeasureId])
+REFERENCES [dbo].[Measures] ([Id])
+GO
+ALTER TABLE [dbo].[Votings]  WITH CHECK ADD FOREIGN KEY([MeasureId])
+REFERENCES [dbo].[Measures] ([Id])
+GO
+ALTER TABLE [dbo].[Votings]  WITH CHECK ADD FOREIGN KEY([OptionId])
+REFERENCES [dbo].[Options] ([Id])
+GO
+ALTER TABLE [dbo].[Votings]  WITH CHECK ADD FOREIGN KEY([UserId])
+REFERENCES [dbo].[Users] ([Id])
+GO
